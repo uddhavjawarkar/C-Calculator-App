9a2a3c01b31ef178f1a424112be10093bb376016
Uddhav Jawarkar <105056986+uddhavjawarkar@users.noreply.github.com>
2022-08-17 22:28:42 +0530

    First Commit

--- Form1.cs
+++ Form1.cs
@@ -1,224 +1,39 @@
-namespace MyCalculator
+namespace Calculator
 {
-    public partial class Calculatorform : Form
+    public partial class Calculator : Form
     {
-        string op;
-        string num1;
-        bool k = false;
-        public Calculatorform()
+        private object textBox_Result;
+
+        public Calculator()
         {
             InitializeComponent();
         }
 
-        private void button12_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            op = "+";
-            num1 = textBox1.Text;
-            textBox1.Text = "+";
 
-
-        }
-
-        private void button10_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-            {
-                textBox1.Text = "";
-                k = false;
-            }
-            textBox1.Text = textBox1.Text + "1";
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "2";
-            }
-
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "3";
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "4";
-            }
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "5";
-            }
+
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "6";
-            }
+            
         }
 
-        private void button20_Click(object sender, EventArgs e)
+        private void button_Click(object sender, EventArgs e)
         {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "7";
-            }
+            textBox_Result.Text = +"1";
         }
 
-        private void button19_Click(object sender, EventArgs e)
-        {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "8";
-            }
-        }
-
-        private void button18_Click(object sender, EventArgs e)
-        {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "9";
-            }
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + "0";
-            }
-        }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-            {
-                if (textBox1.Text == "+" || textBox1.Text == "-" || textBox1.Text == "*" || textBox1.Text == "/" || k == true)
-                {
-                    textBox1.Text = "";
-                    k = false;
-                }
-                textBox1.Text = textBox1.Text + ".";
-            }
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            op = "-";
-            num1 = textBox1.Text;
-            textBox1.Text = "-";
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            op = "*";
-            num1 = textBox1.Text;
-            textBox1.Text = "*";
-
-        }
-
-        private void button17_Click(object sender, EventArgs e)
-        {
-            op = "/";
-            num1 = textBox1.Text;
-            textBox1.Text = "/";
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            switch (op)
-            {
-                case "+":
-                    {
-                        double s = double.Parse(num1) + double.Parse(textBox1.Text);
-                        textBox1.Text = s.ToString();
-                        k = true;
-                        break;
-                    }
-                case "-":
-                    {
-                        double s = double.Parse(num1) - double.Parse(textBox1.Text);
-                        textBox1.Text = s.ToString();
-                        k = true;
-                        break;
-                    }
-                case "*":
-                    {
-                        double s = double.Parse(num1) * double.Parse(textBox1.Text);
-                        textBox1.Text = s.ToString();
-                        k = true;
-                        break;
-                    }
-                case "/":
-                    {
-                        double s = double.Parse(num1) / double.Parse(textBox1.Text);
-                        textBox1.Text = s.ToString();
-                        k = true;
-                        break;
-                    }
-            }
-        }
-
-        private void button16_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = "0";
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            textBox1.Text="0";
-            double s = 0;
-
-        }
+        
     }
 }
